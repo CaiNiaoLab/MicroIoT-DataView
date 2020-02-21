0d6a701a2fbeacb92185e7abc36eeb923cfa189b
fly3366 <1076260338@qq.com>
2020-02-22 02:24:38 +0800

    add: components switch

--- src/components/dashboard/dataset.tsx
+++ src/components/dashboard/dataset.tsx
@@ -2,8 +2,8 @@
 
 import React from "react";
 import styled from "styled-components";
-import { FormSelect } from "@/publicComponents/form";
-import { formComponentConfigPath } from "@/utils/globalVars";
+import {FormSelect} from "@/publicComponents/form";
+import {formComponentConfigPath} from "@/utils/globalVars";
 
 const Container = styled.div`
   height: 100%;
@@ -12,22 +12,22 @@ const Container = styled.div`
 `;
 
 export default class Dataset extends React.Component<any> {
-  render() {
-    const { componentId } = this.props;
-    return (
-      <Container>
-        data
-        {/* <FormSelect
-          items={[
-            {
-              key: "test",
-              value: "test1",
-            },
-          ]}
-          model={formComponentConfigPath + componentId + ".dataset.datasetId"}
-          label=""
-        ></FormSelect> */}
-      </Container>
-    );
-  }
+    render() {
+        const {componentId} = this.props;
+        return (
+            <Container>
+                {componentId ? (<FormSelect
+                    items={[
+                        {
+                            key: "test",
+                            value: "test1",
+                        },
+                    ]}
+                    model={formComponentConfigPath + componentId + ".dataset.dataMap.datasetId"}
+                    label="233"
+                    activeItem={"test"}
+                ></FormSelect>) : (<>Select a component to start</>)}
+            </Container>
+        );
+    }
 }
--- src/store/reducers/componentStateManager.ts
+++ src/store/reducers/componentStateManager.ts
@@ -44,7 +44,7 @@ const defaultState = {
 };
 
 const selectComponentHandle: reducerFunc = (state, actions) => {
-  const { componentId } = actions.payload;
+  const componentId = actions.payload.toString();
   const changeSelectStatus = (componentId: keyof action.ComponentsMap) => {
     // eslint-disable-next-line @typescript-eslint/no-unused-expressions
     state.components[componentId].isSelected = !state.components[componentId]
@@ -55,6 +55,16 @@ const selectComponentHandle: reducerFunc = (state, actions) => {
   } else {
     changeSelectStatus(componentId);
   }
+  //保存current的参数
+  //取消其他选中， 并且focus当前目标
+  for (let componentsKey in state.components) {
+    if (componentsKey != componentId) {
+      state.components[componentsKey].isSelected = false
+    }
+  }
+  //设置可drag
+  state.currentComponentId = componentId;
+  //更新右侧menu的参数
   return state;
 };
 
