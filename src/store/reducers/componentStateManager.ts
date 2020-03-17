0e871b5737b5bbe0ce680c8d6fce7a3dc5424efd
ashes <792634614@qq.com>
2020-03-17 22:17:26 +0800

    update

--- src/store/reducers/componentStateManager.ts
+++ src/store/reducers/componentStateManager.ts
@@ -2,37 +2,9 @@
 
 import * as action from "../actions/actionType";
 import { produce } from "immer";
+import { defaultStateType, actionsType } from "@/store/reducers/types";
+import selectComponentHandle from '@/store/reducers/reducerHandles/selectComponent';
 
-interface actionPayloadType
-  extends action.ComponentsProperty,
-    action.ChangeComponentBoundType,
-    action.ComponentsOption,
-    action.ComponentsStyle,
-    action.AddNewComponent,
-    action.ComponentRect,
-    action.selectComponent {}
-
-interface actionsType {
-  type: string;
-  payload: actionPayloadType;
-}
-
-export interface defaultStateType {
-  canvasId: string;
-  canvasHeigh: string | number;
-  canvasWidth: string | number;
-  canvasPart: string;
-  currentComponentId:
-    | keyof action.ComponentsMap
-    | (keyof action.ComponentsMap)[]
-    | null;
-  componentsIds: (keyof action.ComponentsMap)[];
-  components: action.ComponentsMap;
-}
-
-interface reducerFunc {
-  (state: defaultStateType, actions: actionsType): defaultStateType;
-}
 const defaultState = {
   canvasId: "",
   canvasHeigh: "",
@@ -41,31 +13,6 @@ const defaultState = {
   currentComponentId: null,
   componentsIds: [],
   components: {},
-};
-
-const selectComponentHandle: reducerFunc = (state, actions) => {
-  const componentId = actions.payload.toString();
-  const changeSelectStatus = (componentId: keyof action.ComponentsMap) => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-    state.components[componentId].isSelected = !state.components[componentId]
-      .isSelected;
-  };
-  if (Array.isArray(componentId)) {
-    componentId.map(item => changeSelectStatus);
-  } else {
-    changeSelectStatus(componentId);
-  }
-  //保存current的参数
-  //取消其他选中， 并且focus当前目标
-  for (let componentsKey in state.components) {
-    if (componentsKey != componentId) {
-      state.components[componentsKey].isSelected = false
-    }
-  }
-  //设置可drag
-  state.currentComponentId = componentId;
-  //更新右侧menu的参数
-  return state;
 };
 
 export const componentStateManager = produce(
@@ -91,7 +38,6 @@ export const componentStateManager = produce(
       }
       case action.UPDATE_COMPONENT_RECT: {
         const currentID = state.currentComponentId;
-        // const { rect } = state.components[currentID].property.option;
         let rect;
         if (typeof currentID === "string") {
           rect = state.components[currentID].property.option;
@@ -100,21 +46,10 @@ export const componentStateManager = produce(
             ...actions.payload,
           };
         }
-        // if(Array.isArray(currentID)){
-        //   currentID.map((key:keyof action.ComponentsMap)=>{
-
-        //   })
-        // }
-
         return state;
       }
       case action.SELECT_COMPONENT: {
         selectComponentHandle(state, actions);
-        // const { isLock, isSelected } = state.components[componentId];
-        // if (!isLock) {
-        //   // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-        //   state.components[componentId].isSelected = !isSelected;
-        // }
         break;
       }
       default:
